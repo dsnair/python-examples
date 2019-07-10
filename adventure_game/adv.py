13afc8c53ebfac80ed20f385429df6b3114747e1
Divya Nair <divnairs@gmail.com>
2019-07-10 14:58:53 -0700

    print player's current room

--- adventure_game/adv.py
+++ adventure_game/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 room = {
@@ -33,6 +34,8 @@ room['treasure'].s_to = room['narrow']
 
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("outside")
+print(player)
 
 # Write a loop that:
 # * Prints the current room name
