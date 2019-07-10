e145f43cf492206686cd8d9103c32825cde1322d
Divya Nair <divnairs@gmail.com>
2019-07-09 17:29:29 -0700

    print sys.argv one per line

--- python1/03_modules.py
+++ python1/03_modules.py
@@ -9,7 +9,8 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-print("Script name:",sys.argv)
+for item in sys.argv:
+    print("Argv:", item)
 
 # Print out the OS platform you're using:
 print("Mac OS:", sys.platform)
