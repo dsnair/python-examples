20ca52a9dc4cda1a88b4bb2f187e455f9c9bebb6
Divya Nair <divnairs@gmail.com>
2019-07-12 10:10:57 -0700

    finished OOP

--- sprint/oop/oop1.py
+++ sprint/oop/oop1.py
@@ -1,5 +1,5 @@
 # Write classes for the following class hierarchy:
-#
+
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
 #      v                v
@@ -7,13 +7,37 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
-#
+
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
-#
+
 # e.g.
-#
 # class Whatever:
 #     pass
-#
+
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    # base class
+    pass
+
+# inherits from Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+# inherits from FlightVehicle
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+# inherits from GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
